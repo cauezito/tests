16d0570aff1c8f992d5fe0971ec43d8278a08bde
Cauê Santos <caah.sfc@live.com>
2020-12-10 11:23:44 -0300

    Instantiating classes before each test

--- src/test/java/br/com/cauezito/tests/MathUtilsTest.java
+++ src/test/java/br/com/cauezito/tests/MathUtilsTest.java
@@ -2,25 +2,31 @@ package br.com.cauezito.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+	
+	MathUtils math;
+	
+	/* Execute before each test run */
+	@BeforeEach
+	void init() {
+		math = new MathUtils();
+	}
 
 	@Test
 	void addTest() {
-		MathUtils math = new MathUtils();		
-		assertEquals(20, math.add(15, 10), "This method should add two numbers");
+		assertEquals(25, math.add(15, 10), "This method should add two numbers");
 	}
 	
 	@Test
 	void divideTest() {
-		MathUtils math = new MathUtils();	
 		assertThrows(ArithmeticException.class, () -> math.divide(5,0), "This method should divide by zero and generate an error");
 	}
 	
 	@Test
 	void computeCircleTest() {
-		MathUtils math = new MathUtils();	
 		double expected = 314.1592653589793;
 		double actual = math.computeCircleArea(10);
 		assertEquals(expected, actual, "This method should return the correct area of ​​the circle");
